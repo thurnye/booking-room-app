a8cb1912e03f76851b901acc0bd33bfba55460df
Daniel Tamunotonye <dan_tonye@Gudi.local>
2021-06-13 22:37:25 -0400

    view

--- src/components/eventCalendar/event-utils.js
+++ src/components/eventCalendar/event-utils.js
@@ -1,14 +1,8 @@
-import { useSelector } from 'react-redux';
 let eventGuid = 0
 let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
 
 
-// function Events(){
-//   const myReservations = useSelector((state) => state.newReservations);
-//   console.log(myReservations)
-//   // store item in local storage
-  
-// }
+
 export const INITIAL_EVENTS = [
   {
     id: createEventId(),
